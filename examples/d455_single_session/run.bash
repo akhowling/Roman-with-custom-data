76ee847909fe4648170b351006d94e827fa3e5f9
Mason Peterson <mbpeterson70@gmail.com>
2025-06-09 22:59:49 -0400

    handle setting default envs outside of tmuxp

--- examples/d455_single_session/run.bash
+++ examples/d455_single_session/run.bash
@@ -5,5 +5,7 @@ export ROS_IP=127.0.0.1
 export ROS_MASTER_URI=http://$ROS_IP:11311
 export ROMAN_ROS_WS=$(realpath $SCRIPT_DIR/../../../../)
 export KIMERA_VIO_ROS2_REALSENSE_DIR=$ROMAN_ROS_WS/src/kimera-vio-ros2-realsense
+export OUTPUT_DIR=~/.roman_ros2
+export RECORD=false
 
 tmuxp load $SCRIPT_DIR/d455_online.yaml
